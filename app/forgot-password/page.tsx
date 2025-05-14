8816d71a98d1991120db8a050e8c47bb449398d3
ahmednaser111 <ahmednaser7707@gmail.com>
2025-05-14 22:16:25 +0300

    add "use client" directive to forgot password page for client-side rendering

--- app/forgot-password/page.tsx
+++ app/forgot-password/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from "react";
 import ForgotPasswordForm from "../Components/auth/ForgotPasswordForm/ForgotPasswordForm";
 import useRedirect from "@/hooks/useUserRedirect";
