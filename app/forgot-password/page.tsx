a820068b6aeeba9a752a6b7b93e8c66476b64d1b
ahmednaser111 <ahmednaser7707@gmail.com>
2025-05-15 19:32:09 +0300

    remove unused useRedirect hook import from forgot password page

--- app/forgot-password/page.tsx
+++ app/forgot-password/page.tsx
@@ -2,10 +2,10 @@
 
 import React from "react";
 import ForgotPasswordForm from "../Components/auth/ForgotPasswordForm/ForgotPasswordForm";
-import useRedirect from "@/hooks/useUserRedirect";
+// import useRedirect from "@/hooks/useUserRedirect";
 
 function page() {
-	useRedirect(true); // auth page
+	// useRedirect(true); // auth page
 	return (
 		<div className='auth-page w-full h-full flex justify-center items-center dark:bg-gray-800'>
 			<ForgotPasswordForm />
